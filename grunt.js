cec1bc233bf8d665c0fbefbf23566c1e20a57fe9
Scott González <scott.gonzalez@gmail.com>
2013-07-25 13:55:49 -0400

    Use Handlebars to generate listings.

--- grunt.js
+++ grunt.js
@@ -3,7 +3,8 @@ module.exports = function( grunt ) {
 "use strict";
 
 var _ = require( "underscore" ),
-	semver = require( "semver" );
+	semver = require( "semver" ),
+	Handlebars = require( "handlebars" );
 
 grunt.loadNpmTasks( "grunt-clean" );
 grunt.loadNpmTasks( "grunt-html" );
@@ -101,21 +102,6 @@ function parseFiles( files, regex ) {
 		.sort( sortVersion );
 }
 
-function listItem( prefix, version, files ) {
-	var li = "<li>" + prefix + " " + version + " - ";
-
-	Object.keys( files ).forEach(function( type, index ) {
-		if ( index ) {
-			li += " or ";
-		}
-
-		li += "<a href='/" + files[ type ].filename + "'>" + type + "</a> ";
-	});
-	li += "</li>";
-
-	return li;
-}
-
 function getLatestStable( organized ) {
 	return _.find( Object.keys( organized ), function( version ) {
 		return version.indexOf( "-" ) === -1;
@@ -125,8 +111,7 @@ function getLatestStable( organized ) {
 var rjQueryVersion = /(jquery-(\d+\.\d+(?:\.\d+)?[^.]*)(?:\.(min|pack))?\.js)/;
 var rjQueryMigrateVersion = /(jquery-migrate-(\d+\.\d+(?:\.\d+)?[^.]*)(?:\.min)?\.js)/;
 grunt.registerTask( "build-index", function() {
-	var mainIndex, projectIndex, latestStable, gitListing,
-		files = grunt.file.expandFiles( "cdn/**.js" ),
+	var files = grunt.file.expandFiles( "cdn/**.js" ),
 		jQueryCoreFiles = parseFiles( files, rjQueryVersion ),
 		jQueryMigrateFiles = parseFiles( files, rjQueryMigrateVersion ),
 		jQuery2 = organize( jQueryCoreFiles.filter(function( match ) {
@@ -135,68 +120,55 @@ grunt.registerTask( "build-index", function() {
 		jQuery1 = organize( jQueryCoreFiles.filter(function( match ) {
 			return semver.satisfies( match.version, "1.x" );
 		})),
-		jQueryMigrate = organize( jQueryMigrateFiles );
+		jQueryMigrate = organize( jQueryMigrateFiles ),
+		data = {
+			jquery2: {
+				latestStable: jQuery2[ getLatestStable( jQuery2 ) ],
+				all: jQuery2
+			},
+			jquery1: {
+				latestStable: jQuery1[ getLatestStable( jQuery1 ) ],
+				all: jQuery1
+			},
+			migrate: {
+				latestStable: jQueryMigrate[ getLatestStable( jQueryMigrate ) ],
+				all: jQueryMigrate
+			}
+		};
 
-	mainIndex = "<script>{\"title\":\"jQuery CDN - Latest Versions\"}</script>\n";
-	mainIndex += "<h2>jQuery Core</h2>";
-	mainIndex += "<p>Showing the latest release in each major branch. <a href='/jquery/'>See all versions of jQuery Core</a>.</p>";
+	Handlebars.registerHelper( "listItem", function( prefix, files ) {
+		var li = "<li>";
+		Object.keys( files ).forEach(function( type, index ) {
+			if ( !index ) {
+				li += prefix + " " + files[ type ].version + " - ";
+			} else {
+				li += " or ";
+			}
 
-	latestStable = getLatestStable( jQuery2 );
-	mainIndex += "<h3>jQuery 2.x - Latest Version (IE<9 not supported)</h3>";
-	mainIndex += "<ul>";
-	mainIndex += listItem( "jQuery Core", latestStable, jQuery2[ latestStable ] );
-	mainIndex += "</ul>";
+			li += "<a href='/" + files[ type ].filename + "'>" + type + "</a> ";
+		});
+		li += "</li>";
 
-	latestStable = getLatestStable( jQuery1 );
-	mainIndex += "<h3>jQuery 1.x - Latest Version</h3>";
-	mainIndex += "<ul>";
-	mainIndex += listItem( "jQuery Core", latestStable, jQuery1[ latestStable ] );
-	mainIndex += "</ul>";
-
-	gitListing = "<h3>jQuery - Live git versions</h3>";
-	gitListing += "<p>UNSTABLE, NOT FOR PRODUCTION</p>";
-	gitListing += "<ul>";
-	gitListing += "<li><a href='/jquery-git2.js'>jQuery 2.x git build</a> - <a href='/jquery-git2.min.js'>minified</a></li>";
-	gitListing += "<li><a href='/jquery-git1.js'>jQuery 1.x git build</a> - <a href='/jquery-git1.min.js'>minified</a></li>";
-	gitListing += "<li><a href='/jquery-migrate-git.js'>jQuery Migrate git build</a> - <a href='/jquery-migrate-git.min.js'>minified</a></li>";
-	gitListing += "</ul>";
-
-	mainIndex += gitListing;
-
-	latestStable = getLatestStable( jQueryMigrate );
-	mainIndex += "<h3>jQuery Migrate - Latest Version</h3>";
-	mainIndex += "<ul>";
-	mainIndex += listItem( "jQuery Migrate", latestStable, jQueryMigrate[ latestStable ] );
-	mainIndex += "</ul>";
-
-
-	// Build /jquery/
-	projectIndex = "<script>{\"title\":\"jQuery Core - All Versions\"}</script>\n";
-	projectIndex += gitListing;
-
-	projectIndex += "<h2>jQuery Core - All 2.x Versions</h2>";
-	projectIndex += "<ul>";
-	Object.keys( jQuery2 ).forEach(function( version ) {
-		projectIndex += listItem( "jQuery Core", version, jQuery2[ version ] );
+		return new Handlebars.SafeString( li );
 	});
-	projectIndex += "</ul>";
 
-	projectIndex += "<h2>jQuery Core - All 1.x Versions</h2>";
-	projectIndex += "<ul>";
-	Object.keys( jQuery1 ).forEach(function( version ) {
-		projectIndex += listItem( "jQuery Core", version, jQuery1[ version ] );
-	});
-	projectIndex += "</ul>";
+	Handlebars.registerHelper( "include", (function() {
+		var templates = {};
+		return function( template ) {
+			if ( !templates.hasOwnProperty( template ) ) {
+				templates[ template ] = Handlebars.compile(
+					grunt.file.read( "templates/" + template + ".hbs" ) );
+			}
 
-	projectIndex += "<h2>jQuery Migrate - All Versions</h2>";
-	projectIndex += "<ul>";
-	Object.keys( jQueryMigrate ).forEach(function( version ) {
-		projectIndex += listItem( "jQuery Migrate", version, jQueryMigrate[ version ] );
-	});
-	projectIndex += "</ul>";
-	grunt.file.write( "dist/wordpress/posts/page/jquery.html", projectIndex );
+			return new Handlebars.SafeString( templates[ template ]() );
+		};
+	})());
 
-	grunt.file.write( "dist/wordpress/posts/page/index.html", mainIndex );
+	grunt.file.write( "dist/wordpress/posts/page/index.html",
+		Handlebars.compile( grunt.file.read( "templates/index.hbs" ) )( data ) );
+
+	grunt.file.write( "dist/wordpress/posts/page/jquery.html",
+		Handlebars.compile( grunt.file.read( "templates/jquery.hbs" ) )( data ) );
 });
 
 };
