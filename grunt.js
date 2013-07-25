6b37b87877568b1becae79cdc7d7c5f1c64b83c9
Scott González <scott.gonzalez@gmail.com>
2013-07-25 14:15:43 -0400

    Move helper functions inside their associated task.

--- grunt.js
+++ grunt.js
@@ -49,71 +49,67 @@ grunt.registerTask( "default", "build-wordpress" );
 grunt.registerTask( "build", "build-pages build-resources build-index" );
 grunt.registerTask( "build-wordpress", "check-modules clean lint build" );
 
-
-var rversion = /^(\d+)\.(\d+)(?:\.(\d+))?-?(.*)$/;
-function getVersion( version ) {
-	var match = rversion.exec( version );
-
-	return match[ 1 ] + "." + match[ 2 ] + "." + ( match[ 3 ] || 0 ) +
-		( match[ 4 ] ? "-" + match[ 4 ] : "" );
-}
-
-function sortVersion( a, b ) {
-	return semver.compare( b.version, a.version ) || ( a.type < b.type ? -1 : 1 );
-}
-
-function organize( files ) {
-	var result = {};
-
-	files.forEach(function( file ) {
-		var version = result[ file.version ] || ( result[ file.version ] = {} );
-		version[ file.typePretty ] = file;
-	});
-
-	return result;
-}
-
-var prettyTypes = {
-	"": "uncompressed",
-	"min": "minified",
-	"pack": "packed"
-};
-
-function parseFiles( files, regex ) {
-	return files
-		.map(function( filename ) {
-			var type,
-				matches = regex.exec( filename );
-
-			if ( !matches ) {
-				return null;
-			}
-
-			type = matches[ 3 ] || "";
-			return {
-				filename: matches[ 0 ],
-				version: getVersion( matches[ 2 ] ),
-				type: type,
-				typePretty: prettyTypes[ type ]
-			};
-		})
-		// Remove null values from filtering
-		.filter( _.identity )
-		.sort( sortVersion );
-}
-
-function getLatestStable( organized ) {
-	return _.find( Object.keys( organized ), function( version ) {
-		return version.indexOf( "-" ) === -1;
-	});
-}
-
-var rjQueryVersion = /(jquery-(\d+\.\d+(?:\.\d+)?[^.]*)(?:\.(min|pack))?\.js)/;
-var rjQueryMigrateVersion = /(jquery-migrate-(\d+\.\d+(?:\.\d+)?[^.]*)(?:\.min)?\.js)/;
 grunt.registerTask( "build-index", function() {
-	var files = grunt.file.expandFiles( "cdn/**.js" ),
-		jQueryCoreFiles = parseFiles( files, rjQueryVersion ),
-		jQueryMigrateFiles = parseFiles( files, rjQueryMigrateVersion ),
+	var rversion = /^(\d+)\.(\d+)(?:\.(\d+))?-?(.*)$/;
+	function getVersion( version ) {
+		var match = rversion.exec( version );
+
+		return match[ 1 ] + "." + match[ 2 ] + "." + ( match[ 3 ] || 0 ) +
+			( match[ 4 ] ? "-" + match[ 4 ] : "" );
+	}
+
+	function organize( files ) {
+		var result = {};
+
+		files.forEach(function( file ) {
+			var version = result[ file.version ] || ( result[ file.version ] = {} );
+			version[ file.typePretty ] = file;
+		});
+
+		return result;
+	}
+
+	var prettyTypes = {
+		"": "uncompressed",
+		"min": "minified",
+		"pack": "packed"
+	};
+	function parseFiles( files, regex ) {
+		return files
+			.map(function( filename ) {
+				var type,
+					matches = regex.exec( filename );
+
+				if ( !matches ) {
+					return null;
+				}
+
+				type = matches[ 3 ] || "";
+				return {
+					filename: matches[ 0 ],
+					version: getVersion( matches[ 2 ] ),
+					type: type,
+					typePretty: prettyTypes[ type ]
+				};
+			})
+			// Remove null values from filtering
+			.filter( _.identity )
+			.sort(function( a, b ) {
+				return semver.compare( b.version, a.version ) || ( a.type < b.type ? -1 : 1 );
+			});
+	}
+
+	function getLatestStable( organized ) {
+		return _.find( Object.keys( organized ), function( version ) {
+			return version.indexOf( "-" ) === -1;
+		});
+	}
+
+	var allFiles = grunt.file.expandFiles( "cdn/**.js" ),
+		jQueryCoreFiles = parseFiles( allFiles,
+			/(jquery-(\d+\.\d+(?:\.\d+)?[^.]*)(?:\.(min|pack))?\.js)/ ),
+		jQueryMigrateFiles = parseFiles( allFiles,
+			/(jquery-migrate-(\d+\.\d+(?:\.\d+)?[^.]*)(?:\.min)?\.js)/ ),
 		jQuery2 = organize( jQueryCoreFiles.filter(function( match ) {
 			return semver.satisfies( match.version, "2.x" );
 		})),
