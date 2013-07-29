21daade6c276aa7c2521420777c5cc01e1fe60ed
Scott González <scott.gonzalez@gmail.com>
2013-07-29 19:47:56 -0400

    Coding standards.

--- purge.php
+++ purge.php
@@ -1,53 +1,45 @@
 <?php
-//requires https://github.com/netdna/netdnarws-php
-require_once('netdnarws-php/NetDNA.php');
+// requires https://github.com/netdna/netdnarws-php
+require_once( 'netdnarws-php/NetDNA.php' );
 
-$config = json_decode(file_get_contents('./config.json'), true);
-$config = $config["cdn"];
+$config = json_decode( file_get_contents( './config.json' ), true );
+$config = $config[ 'cdn' ];
+$zone_id = $config[ 'zone_id' ];
 
-//place your alias, key, secret into this constructor
-$api = new NetDNA($config["alias"], $config["consumer_key"], $config["consumer_secret"]);
+function purgeCacheFileFromCDN( $id, $files = null ) {
+	$api = new NetDNA( $config[ 'alias' ], $config[ 'consumer_key' ], $config[ 'consumer_secret' ] );
+	$result = null;
 
-function purgeCacheFileFromCDN($id, $files = null) {
+	if ( empty( $files ) ) {
+		$result = $api->delete( '/zones/pull.json/' . $id . '/cache' );
+	} else if ( !is_array( $files ) ) {
+		// Purge single file
+		$params = array( 'file' => $files );
+		$result = $api->delete( '/zones/pull.json/' . $id . '/cache', $params );
+	} else if ( is_array( $files ) ) {
+		// Purge multiple files
+		$params = array();
+		foreach ( $files as $k => $v ) $params[ $k ] = $v;
+		$result = $api->delete( '/zones/pull.json/' . $id . '/cache', $params );
+	}
 
-    global $api;
-    //3 options for purge
-    $result = null;
-    
-    if ($files == null){
-        $result = $api->delete('/zones/pull.json/'.$id.'/cache');
-    } else if (!is_array($files)){
-        //Purge single file
-        $params = array('file'=>$files);
-        $result = $api->delete('/zones/pull.json/'.$id.'/cache',$params);
-    } else if (is_array($files)){
-        //Purge multiple files
-        $params = array();
-        foreach ($files as $k=>$v) $params[$k] = $v;
-        $result = $api->delete('/zones/pull.json/'.$id.'/cache',$params);
-    }
-
-    $result = json_decode($result,true);
-    if ($result['code'] != 200) {
-        echo 'Error reported: '.print_r( $result, 1 );
-    } else {
-        echo "Done\n";
-    }
-
+	$result = json_decode( $result, true );
+	if ( $result[ 'code' ] !== 200 ) {
+		echo 'Error reported: ' . print_r( $result, true );
+	} else {
+		echo 'Done\n';
+	}
 }
 
-//set zone id that you want to purge
-$zone_id = $config["zone_id"];
-
-$parts = preg_split( "/\?reload=?/", $_SERVER["REQUEST_URI"] );
+$parts = preg_split( '/\?reload=?/', $_SERVER[ 'REQUEST_URI' ] );
 if ( !$parts ) {
-  header("400 Bad Request");
-  echo $_SERVER["REQUEST_URI"]." is not a valid URI";
-  exit;
+	header( '400 Bad Request' );
+	echo $_SERVER[ 'REQUEST_URI' ] . ' is not a valid URI.';
+	exit;
 }
 
-$file = $parts[0];
+$file = $parts[ 0 ];
 
-header( "Content-Type: text/plain" );
-echo "Attempting to purge: ".$zone_id.": ".$file."\n";
+header( 'Content-Type: text/plain' );
+echo "Attempting to purge: $zone_id: $file.\n";
 purgeCacheFileFromCDN( $zone_id, $file );
