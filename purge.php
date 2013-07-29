3ec6b5f23ce6db3ca4a91cdedb4444774fcfcdad
Scott González <scott.gonzalez@gmail.com>
2013-07-29 19:53:07 -0400

    Simplify purge script.

--- purge.php
+++ purge.php
@@ -1,35 +1,9 @@
 <?php
-// requires https://github.com/netdna/netdnarws-php
 require_once( 'netdnarws-php/NetDNA.php' );
 
 $config = json_decode( file_get_contents( './config.json' ), true );
 $config = $config[ 'cdn' ];
-$zone_id = $config[ 'zone_id' ];
-
-function purgeCacheFileFromCDN( $id, $files = null ) {
-	$api = new NetDNA( $config[ 'alias' ], $config[ 'consumer_key' ], $config[ 'consumer_secret' ] );
-	$result = null;
-
-	if ( empty( $files ) ) {
-		$result = $api->delete( '/zones/pull.json/' . $id . '/cache' );
-	} else if ( !is_array( $files ) ) {
-		// Purge single file
-		$params = array( 'file' => $files );
-		$result = $api->delete( '/zones/pull.json/' . $id . '/cache', $params );
-	} else if ( is_array( $files ) ) {
-		// Purge multiple files
-		$params = array();
-		foreach ( $files as $k => $v ) $params[ $k ] = $v;
-		$result = $api->delete( '/zones/pull.json/' . $id . '/cache', $params );
-	}
-
-	$result = json_decode( $result, true );
-	if ( $result[ 'code' ] !== 200 ) {
-		echo 'Error reported: ' . print_r( $result, true );
-	} else {
-		echo 'Done\n';
-	}
-}
+$zone_id = 1;
 
 $parts = preg_split( '/\?reload=?/', $_SERVER[ 'REQUEST_URI' ] );
 if ( !$parts ) {
@@ -42,4 +16,20 @@ $file = $parts[ 0 ];
 
 header( 'Content-Type: text/plain' );
 echo "Attempting to purge: $zone_id: $file.\n";
-purgeCacheFileFromCDN( $zone_id, $file );
+
+$api = new NetDNA( 'jquery', $config[ 'consumer_key' ], $config[ 'consumer_secret' ] );
+
+if ( empty( $file ) ) {
+	// Purge all files
+	$result = $api->delete( '/zones/pull.json/' . $zone_id . '/cache' );
+} else {
+	// Purge single file
+	$result = $api->delete( '/zones/pull.json/' . $zone_id . '/cache', array( 'file' => $file ) );
+}
+
+$result = json_decode( $result, true );
+if ( $result[ 'code' ] !== 200 ) {
+	echo 'Error reported: ' . print_r( $result, true );
+} else {
+	echo 'Done\n';
+}
