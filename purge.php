1780c44c92def761c0618a8fd36177d57f749114
Scott González <scott.gonzalez@gmail.com>
2013-07-30 14:44:50 -0400

    Only allow purging a single file.

--- purge.php
+++ purge.php
@@ -18,18 +18,11 @@ header( 'Content-Type: text/plain' );
 echo "Attempting to purge: $zone_id: $file.\n";
 
 $api = new NetDNA( 'jquery', $config[ 'consumer_key' ], $config[ 'consumer_secret' ] );
-
-if ( empty( $file ) ) {
-	// Purge all files
-	$result = $api->delete( '/zones/pull.json/' . $zone_id . '/cache' );
-} else {
-	// Purge single file
-	$result = $api->delete( '/zones/pull.json/' . $zone_id . '/cache', array( 'file' => $file ) );
-}
-
+$result = $api->delete( '/zones/pull.json/' . $zone_id . '/cache', array( 'file' => $file ) );
 $result = json_decode( $result, true );
+
 if ( $result[ 'code' ] !== 200 ) {
 	echo 'Error reported: ' . print_r( $result, true );
 } else {
-	echo 'Done\n';
+	echo "Done\n";
 }
