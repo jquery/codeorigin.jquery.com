4fd831c835eae583842ae0131e1ad9ce5ec56deb
Timo Tijhof <krinklemail@gmail.com>
2018-08-25 02:41:59 +0100

    Build: Only require config.json for the "deploy" task
    This makes the other tasks also usable locally, given they
    don't use the wordpress config. In prep for fixing issue #40.

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,11 +9,6 @@ grunt.loadNpmTasks( "grunt-jquery-content" );
 grunt.loadNpmTasks( "grunt-sri" );
 
 grunt.initConfig( {
-	wordpress: (function() {
-		var config = require( "./config" );
-		config.dir = "dist/wordpress";
-		return config;
-	})(),
 	sri: {
 		generate: {
 			src: [
@@ -424,8 +419,22 @@ grunt.registerTask( "ensure-dist-resources", function() {
 	grunt.file.mkdir( "dist/resources" );
 } );
 
+grunt.registerTask( "ensure-wordpress-config", function() {
+	// This will fail with "Cannot find module" if the file
+	// does not exist
+	var config = require( "./config" );
+	config.dir = "dist/wordpress";
+	grunt.config.merge( {
+		wordpress: config
+	} );
+} );
+
 grunt.registerTask( "sri-generate", ["ensure-dist-resources", "sri:generate"] );
+
+// The "grunt deploy" command is automatically invoked on git-commit by the server that
+// will deploy the WordPress site.
+// Task tree: "deploy" > "wordpress-deploy" > "build-wordpress" > "build".
 grunt.registerTask( "build", ["sri-generate", "build-index"] );
-grunt.registerTask( "deploy", ["wordpress-deploy", "reload-listings"] );
+grunt.registerTask( "deploy", ["ensure-wordpress-config", "wordpress-deploy", "reload-listings"] );
 
 };
